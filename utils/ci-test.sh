d1031d9b6b3e31a588fdb8f5e52fbb3a9322fba4
Matias Insaurralde <matias@insaurral.de>
2018-05-23 23:48:37 -0400

    Update Appveyor settings

--- utils/ci-test.sh
+++ utils/ci-test.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
-cd $GOPATH/src/github.com/matiasinsaurralde/go-dotnet/dotnet
+#!/bin/bash
+export GOPATH=/c/gopath
+cd $GOPATH/src/github.com/matiasinsaurralde/appveyor-test/dotnet
+go get
+# cd $GOPATH/src/github.com/matiasinsaurralde/go-dotnet/dotnet
 go test -v
